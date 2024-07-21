a455a6127292c12682e84f1b862358eeb8e7fada
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2024-07-21 13:25:53 +0200

    adding new controller test and some impouvments

--- src/test/java/dominik/nadgodziny/infrastructure/overtime/controller/OvertimeControllerTest.java
+++ src/test/java/dominik/nadgodziny/infrastructure/overtime/controller/OvertimeControllerTest.java
@@ -1,24 +1,16 @@
 package dominik.nadgodziny.infrastructure.overtime.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dominik.nadgodziny.domain.overtime.OvertimeFacade;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
-@WebMvcTest(OvertimeController.class)
-@AutoConfigureMockMvc
 public class OvertimeControllerTest extends OvertimesExamples{
 
     @Autowired
@@ -27,46 +19,49 @@ public class OvertimeControllerTest extends OvertimesExamples{
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
-    private OvertimeFacade overtimeFacade;
-
-
     @BeforeEach
     void setUp() {
-        twoOvertimesExamplesWithStatusZlecenie();
+        // Add initial data to the database
+        overtimesLoopAdder(twoOvertimesExamplesWithStatusZlecenie());
+        overtimesLoopAdder(twoOvertimesExamplesWithStatusNadgodziny());
     }
 
+
     @Test
-    void findAllOvertimes_ReturnsListOfOvertimes() throws Exception {
+    void findAllOvertimesReturnsListOfOvertimes() throws Exception {
         //Given
-        when(overtimeFacade.findAll()).thenReturn(mockOvertimeResponseDtoList);
+
 
         //When
         mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/overtimes")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(2))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(4));
 
         //Then
     }
 
     @Test
-    void findOvertimesByStatus_ReturnsFilteredOvertimes() throws Exception {
-        //Given
-        when(overtimeFacade.findByStatusAndYear(anyInt(), anyString())).thenReturn(mockOvertimeResponseDtoList);
-
-        //When
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/overtimes/status")
-                        .queryParam("year", "2023")
+    void  shouldFindOvertimesByStatusReturnsFilteredOvertimes() throws Exception {
+        //Given &  When
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/overtimes/status")
+                        .queryParam("year", "2024")
                         .queryParam("status", "zlecenie")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(2))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2));
-
+                        .contentType(MediaType.APPLICATION_JSON));
         //Then
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(0));
+    }
+
+    @Test
+    void  shouldFindOvertimesByStatusReturnsFilteredOvertimesAndFindTwo() throws Exception {
+        //Given &  When
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/overtimes/status")
+                .queryParam("year", "2023")
+                .queryParam("status", "zlecenie")
+                .contentType(MediaType.APPLICATION_JSON));
+        //Then
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(2));
     }
 }
--- src/test/java/dominik/nadgodziny/infrastructure/overtime/controller/OvertimesExamples.java
+++ src/test/java/dominik/nadgodziny/infrastructure/overtime/controller/OvertimesExamples.java
@@ -1,24 +1,70 @@
 package dominik.nadgodziny.infrastructure.overtime.controller;
 
+import dominik.nadgodziny.NadgodzinyApplication;
+import dominik.nadgodziny.domain.overtime.OvertimeFacade;
 import dominik.nadgodziny.domain.overtime.dto.OvertimeResponseDto;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
+@SpringBootTest(classes = NadgodzinyApplication.class)
+@ActiveProfiles("integration")
+@Testcontainers
+@AutoConfigureMockMvc
+@Transactional
 public class OvertimesExamples {
 
-    protected  List<OvertimeResponseDto> mockOvertimeResponseDtoList;
+    protected List<OvertimeResponseDto> mockOvertimeResponseDtoList;
+
+    @Autowired
+     OvertimeFacade overtimeFacade;
 
 
-    void twoOvertimesExamplesWithStatusZlecenie() {
-        mockOvertimeResponseDtoList = Arrays.asList(
-                new OvertimeResponseDto(1, LocalDate.parse("2023-01-01"),LocalDate.parse("2023-01-01"), "zlecenie", 8),
-                new OvertimeResponseDto(2, LocalDate.parse("2023-01-01"), LocalDate.parse("2023-01-01"), "zlecenie", 8)
+    List<OvertimeResponseDto> twoOvertimesExamplesWithStatusZlecenie() {
+        return mockOvertimeResponseDtoList = Arrays.asList(
+                new OvertimeResponseDto(0, LocalDate.parse("2023-01-01"), LocalDate.parse("2023-01-01"), "zlecenie", 8),
+                new OvertimeResponseDto(0, LocalDate.parse("2023-01-01"), LocalDate.parse("2023-01-01"), "zlecenie", 8)
         );
     }
 
+    List<OvertimeResponseDto> twoOvertimesExamplesWithStatusNadgodziny() {
+        return mockOvertimeResponseDtoList = Arrays.asList(
+                new OvertimeResponseDto(0, LocalDate.parse("2023-01-01"), LocalDate.parse("2023-01-01"), "nadgodziny", 8),
+                new OvertimeResponseDto(0, LocalDate.parse("2023-01-01"), LocalDate.parse("2023-01-01"), "nadgodziny", 8)
+        );
+    }
 
+    void overtimesLoopAdder(List<OvertimeResponseDto>overtimeResponseDtoList) {
+        for (OvertimeResponseDto overtimeResponseDto : overtimeResponseDtoList) {
+            overtimeFacade.createOvertimeAndSaveToDb(overtimeResponseDto.overtimeDate(),
+                    overtimeResponseDto.status(),
+                    overtimeResponseDto.duration());
+        }
+    }
+
+    @Container
+    public static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    @DynamicPropertySource
+    static void mysqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
 
 
 }
